661ee3d37d4afaa18d71e4247ea5a0d8473c8a87
shreeyag11@gmail.com <shreeyag11@gmail.com>
2020-04-26 00:36:54 -0400

    sfc

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{counter}}</h1>
+        <button @click="counter++">Click me</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        data () {
+            return {
+                counter: 0
+            }
+        }
+    }
+</script>
---
+++ main.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+var app = new Vue({
+    el: '#demo',
+    render: h => h(App)
+})
